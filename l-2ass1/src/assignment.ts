f53b0d22bedeaa78668306993d03dca363df2c2f
Md. Ridoy Islam <138614698+redoy690@users.noreply.github.com>
2025-05-08 23:12:00 +0600

    Add files via upload

---
+++ l-2ass1/src/assignment.ts
@@ -0,0 +1,136 @@
+function formatString(input: string, toUpper?: boolean): string {
+    if (toUpper == true) {
+        return input.toUpperCase()
+    } else if (toUpper == false) {
+        return input.toLowerCase()
+    } else {
+        return input.toUpperCase()
+    }
+
+}
+
+
+
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+    return items.filter(item => item.rating >= 4)
+}
+
+
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+    return arrays.flat()
+}
+
+
+
+
+
+
+class Vehicle {
+    private make: string;
+    private year: number
+
+    constructor(make: string, year: number) {
+        this.make = make;
+        this.year = year
+    }
+
+    getInfo() {
+        return (`Make:${this.make},Year:${this.year} `)
+    }
+}
+
+
+class Car extends Vehicle {
+    private model: string
+
+    constructor(make: string, year: number, model: string) {
+        super(make, year)
+        this.model = model;
+
+    }
+
+    getModel() {
+        return (`Model:${this.model} `)
+    }
+}
+
+
+
+
+function processValue(value: string | number): number {
+    if (typeof value === "string") {
+        return value.length
+    } else {
+        return value * 2
+    }
+}
+
+
+
+
+
+
+interface Product {
+    name: string;
+    price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+    if (products.length === 0) {
+        return null
+    }
+
+    let Expensive = products[0]
+    for (const product of products) {
+        if (product.price > Expensive.price) {
+            Expensive = product
+        }
+    }
+    return Expensive
+}
+
+
+
+
+enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+function getDayType(day: Day): string {
+    switch (day) {
+        case Day.Saturday:
+        case Day.Sunday:
+            return "Weekend";
+        default:
+            return "Weekday";
+    }
+}
+
+
+
+
+
+
+async function squareAsync(n: number): Promise<number> {
+    try {
+      if (n < 0) {
+        throw new Error("Negative number not allowed");
+      }
+  
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      return n * n;
+  
+    } catch (error) {
+      throw error; 
+    }
+  }
+
+
